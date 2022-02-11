0d8b2bf4e6ef6fd8e1cafd23424418ac4fd6c372
Ingrid Aquino <ingridaquinosoares@live.com>
2022-02-11 17:24:27 -0300

    :construction: Create CardFrontBack component without CSS

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,8 +1,8 @@
 import "./src/styles/settings/colors.css";
 import './src/styles/gereric/reset.css';
 import './src/styles/elements/base.css';
-import BoardGame from "./src/objects/BoardGame";
 import Scoreboard from "./src/objects/Scoreboard";
+import BoardGame from "./src/objects/boardGame";
 
 const $root = document.querySelector("#root");
 
@@ -11,7 +11,7 @@ $root.insertAdjacentHTML(
     `
       ${Scoreboard()}
      
-      ${BoardGame(6)} 
+      ${BoardGame(2)} 
     `
     //Definir quantas vezes repetir os cartões.
   );
--- matching-game/src/objects/boardGame/index.js
+++ matching-game/src/objects/boardGame/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
-import CardGame from "../../components/CardGame";
+import CardFrontBack from "../../components/CardFrontBack";
 
 function BoardGame(amountCards) {
-  const $htmlCardGame = CardGame();
-  const $htmlContent = $htmlCardGame.repeat(amountCards); // Para repetir os cartões
+  const $htmlCardFrontBack = CardFrontBack();
+  const $htmlContent = $htmlCardFrontBack.repeat(amountCards); // Para repetir os cartões
 
   return `
     <section class="board-game">
