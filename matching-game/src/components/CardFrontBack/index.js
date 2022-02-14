d7cc3fda2166f3d0e4d803f15dd5030e6e2aa8b2
Ingrid Aquino <ingridaquinosoares@live.com>
2022-02-14 14:20:00 -0300

    :sparkles: Add flip cardGame

--- matching-game/src/components/CardFrontBack/index.js
+++ matching-game/src/components/CardFrontBack/index.js
@@ -4,10 +4,12 @@ import './style.css';
 function CardFrontBack() {
     return`
         <article class="card-front-back">
-            <>
+            <div class="card -front">
                 ${CardGame()}
+            </div>
+            <div class="card -back">
                 ${CardGame("Javascript", "Logo do Javascript")}
-            </>
+            </div>    
 
 
         </article>
